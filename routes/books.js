b935c579c7c2d8f2613727b4f9bdf4e683a39842
SoulX <mrhimanshu305@gmail.com>
2026-05-15 07:33:11 +0530

    Add files via upload

---
+++ routes/books.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/Book");
+
+router.get("/", async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
+});
+
+router.post("/", async (req, res) => {
+  const book = new Book(req.body);
+  await book.save();
+  res.json(book);
+});
+
+router.get("/:id", async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  res.json(book);
+});
+
+router.put("/:id", async (req, res) => {
+  const updated = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Book.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted successfully" });
+});
+
+module.exports = router;
